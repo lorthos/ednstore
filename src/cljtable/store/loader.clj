981418d56a34b96a58f2f9d6b728fb5656622d7d
Halit Olali <halitolali@gmail.com>
2015-04-22 21:30:52 +0200

    loading hash indexes from segment file

--- src/cljtable/store/loader.clj
+++ src/cljtable/store/loader.clj
@@ -1,7 +1,58 @@
-(ns cljtable.store.loader)
+(ns cljtable.store.loader
+  (:require [cljtable.store.reader :as r])
+  (:import (java.nio.channels SeekableByteChannel)))
 
-(defn restore-index
-  "goes through all the keys and re-construct {:key offset} hash index"
-  []
-  ;TODO
+(defn read-next-key-and-offset-and-increment!
+  "given a channel that is at the end position of a record (or at the beginning of the file)
+  1. reads the length of the key
+  2. reads the key
+  3. reads the operation type
+  4. if update - reads the length of the value
+  5. reads the value
+  6. calculates total bytes read returns the key and new offset"
+  [chan offset-atom]
+  ;TODO skip instead of read?
+  (let [old-offset @offset-atom
+        kl (r/read-int-from-chan chan)
+        k (r/read-nippy-from-chan chan kl)
+        op_type (r/read-byte-from-chan chan)]
+    (if (= op_type (byte 41))
+      (let [vl (r/read-int-from-chan chan)
+            v (r/read-nippy-from-chan chan vl)]
+        (do
+          (swap! offset-atom + 4 kl 1 4 vl)
+          {:key k :old-offset old-offset :new-offset @offset-atom}))
+      (do
+        (swap! offset-atom + 4 kl 1)
+        {:key k :old-offset old-offset :new-offset @offset-atom})
+      )))
+
+(defn append-next-line-to-index!
+  "given an index in the atom, append the read result to the index and return the latest offset"
+  [index-atom read-result]
+  (swap! index-atom assoc (:key read-result) (:old-offset read-result))
+  (:new-offset read-result))
+
+(defn load-index
+  "goes through all the keys and re-construct {:key offset} hash index
+  1. position the chan to the beginning offset
+  2. read and assoc accordingly"
+  [^SeekableByteChannel chan]
+  (.position chan 0)
+  (let [offset-atom (atom 0)
+        end (.size chan)
+        index (atom {})]
+    (loop [current 0]
+      (if (= current end)
+        @index
+        (recur (append-next-line-to-index! index (read-next-key-and-offset-and-increment! chan offset-atom)))
+        )
+      )
+    )
   )
+
+(loop [i 5 acc 1]
+  (println i acc)
+  (if (zero? i)
+    acc
+    (recur (dec i) (* acc i))))
---
+++ test/cljtable/store/loader_test.clj
@@ -0,0 +1,50 @@
+(ns cljtable.store.loader-test
+  (:require [clojure.test :refer :all]
+            [cljtable.store.loader :refer :all]
+            [cljtable.store.segment :as s]
+            [cljtable.store.writer :as wrt]))
+
+(defn segment-fixture [f]
+  (s/roll-new-segment! 0)
+  (f)
+  (s/close-active-segment! @s/active-segment))
+
+(use-fixtures :each segment-fixture)
+
+(deftest append-next-line-to-index
+  (testing "with read operation"
+    (let [index (atom {})]
+      ;with an artificial record of k1:v1
+      (is (= 13 (append-next-line-to-index! index {:op :assoc :key "k1" :old-offset 0 :new-offset 13})))
+      (is (= {"k1" 0} @index))
+      ;k2:v2
+      (is (= 26 (append-next-line-to-index! index {:op :assoc :key "k2" :old-offset 13 :new-offset 26})))
+      (is (= {"k1" 0 "k2" 13} @index))
+      ;k1:DELETE
+      (is (= 33 (append-next-line-to-index! index {:op :dissoc :key "k1" :old-offset 26 :new-offset 33})))
+      (is (= {"k1" 26 "k2" 13} @index))
+      )
+    )
+  )
+
+
+;TODO will fail when serialization function changes
+(deftest reconstruct-index
+  (testing "reading-from-actual-segment"
+    (wrt/write! "A" "B" @s/active-segment)
+    (wrt/write! "B" "C" @s/active-segment)
+    (wrt/write! "C" "D" @s/active-segment)
+    (is (= (:read-chan @s/active-segment) (:index @s/active-segment)))
+    (is (= {"A" 0 "B" 33 "C" 66} (load-index (:read-chan @s/active-segment))))
+    (wrt/write! "DD" "EE" @s/active-segment)
+    (wrt/write! "E" "F" @s/active-segment)
+    (is (= (:read-chan @s/active-segment) (:index @s/active-segment)))
+    (is (= {"A" 0 "B" 33 "C" 66 "DD" 99 "E" 134} (load-index (:read-chan @s/active-segment))))
+    (wrt/delete! "E" @s/active-segment)
+    (is (= {"A" 0 "B" 33 "C" 66 "DD" 99 "E" 167} (load-index (:read-chan @s/active-segment))))
+    (wrt/delete! "DD" @s/active-segment)
+    (is (= {"A" 0 "B" 33 "C" 66 "DD" 184 "E" 167} (load-index (:read-chan @s/active-segment))))
+    (println @(:index @s/active-segment))
+    (is (= (:read-chan @s/active-segment) (:index @s/active-segment)))
+    )
+  )
