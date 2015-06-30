6b99f99fd878b8e8efbec16eec3f08f78e8ba621
Halit Olali <halitolali@gmail.com>
2015-06-30 23:17:11 +0200

    bump clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [org.clojure/tools.logging "0.2.6"]
                            [nio "1.0.3"]
                            [com.taoensso/nippy "2.8.0"]])
