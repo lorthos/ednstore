0997e913c018b21c4f4892f689364b2a3f0aeb17
Halit Olali <halitolali@gmail.com>
2016-11-12 23:07:50 +0100

    writer_test

--- test/ednstore/store/writer_test.clj
+++ test/ednstore/store/writer_test.clj
@@ -1,43 +1,43 @@
 (ns ednstore.store.writer-test
   (:require [clojure.test :refer :all]
             [ednstore.store.writer :refer :all]
-            [ednstore.store.segment :as seg]))
+            [ednstore.store.metadata :as md]
+            [ednstore.store.segment :as s]))
 
-(def segment (atom nil))
+(def test-db-ns "writer-test1")
 
-(defn segment-fixture [f]
-  (reset! segment (seg/roll-new-segment! 33))
+(defn writer-fixture [f]
+  (reset! md/store-meta {})
+  (s/roll-new-segment! test-db-ns 0)
   (f)
-  (seg/close-segment! @segment)
-  (reset! seg/active-segment nil))
+  (s/close-segment! (md/get-active-segment-for-namespace test-db-ns)))
 
-(use-fixtures :each segment-fixture)
+(use-fixtures :each writer-fixture)
 
 ;some serialization overhead with nippy
 (deftest put-to-active-segment
   (testing "segment, index and offset management when writing"
     ;index last-offset read-chan write-chan
-    (write! "A" "B" @segment)
-    (write! "AAAA" "BBBB" @segment)
+    (write! test-db-ns "A" "B")
+    (write! test-db-ns "AAAA" "BBBB")
     ;validate index
-    (is (= 2 (count (keys @(:index @segment)))))
-    (is (= 72 @(:last-offset @segment)))
-    (println @(:index @segment))
-    (is (= 0 (get @(:index @segment) "A")))
-    (is (= 33 (get @(:index @segment) "AAAA")))
+    (is (= 2 (count (keys @(:index (md/get-active-segment-for-namespace test-db-ns))))))
+    (is (= 72 @(:last-offset (md/get-active-segment-for-namespace test-db-ns))))
+    (is (= 0 (get @(:index (md/get-active-segment-for-namespace test-db-ns)) "A")))
+    (is (= 33 (get @(:index (md/get-active-segment-for-namespace test-db-ns)) "AAAA")))
     ))
 
 (deftest delete-from-active-segment
   (testing "segment, index and offset management when writing"
     ;index last-offset read-chan write-chan
-    (write! "A" "B" @segment)
-    (write! "AAAA" "BBBB" @segment)
+    (write! test-db-ns "A" "B")
+    (write! test-db-ns "AAAA" "BBBB")
     ;validate index
-    (is (= 2 (count (keys @(:index @segment)))))
-    (delete! "A" @segment)
-    (is (= 2 (count (keys @(:index @segment)))))
+    (is (= 2 (count (keys @(:index (md/get-active-segment-for-namespace test-db-ns))))))
+    (delete! test-db-ns "A")
+    (is (= 2 (count (keys @(:index (md/get-active-segment-for-namespace test-db-ns))))))
     ;TODO side-effect of deleting something that does not exist
-    (delete! "NON_EXISTING_KEY" @segment)
-    (is (= 3 (count (keys @(:index @segment)))))
+    (delete! test-db-ns "NON_EXISTING_KEY" )
+    (is (= 3 (count (keys @(:index (md/get-active-segment-for-namespace test-db-ns))))))
     )
   )
